c72a376b2b55febda42b17c0a454efda517e4fac
System Administrator <root@LEFFI-DA-MACBOOK.local>
2016-11-28 18:37:54 +0000

    first configuration

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myfirstAngularAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the myfirstAngularAppApp
+ */
+angular.module('myfirstAngularAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myfirstAngularAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myfirstAngularAppApp
+ */
+angular.module('myfirstAngularAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
